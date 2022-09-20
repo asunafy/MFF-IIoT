38ea29654e38d8ce99c30b1c135bf249b62f8e13
asunafy <asunafy@126.com>
2022-09-20 11:30:45 +0800

    Create random.h

---
+++ PAT/random.h
@@ -0,0 +1,25 @@
+nes (16 sloc)  351 Bytes
+
+#ifndef RANDOM_H
+#define RANDOM_H
+
+#define RANDOM_DLL
+
+#ifdef RANDOM_DLL
+#define RANDOM_API __declspec(dllexport)			
+#else
+#define RANDOM_API __declspec(dllimport)			
+#endif 
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+int RANDOM_API RAND(int max, int min, int n);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif
